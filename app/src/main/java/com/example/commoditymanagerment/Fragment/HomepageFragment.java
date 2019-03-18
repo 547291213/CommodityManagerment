8e6ef02616d44f560c4fcc1dc7e80ccd5ac5b88c
kefeng <xp134525@foxmail.com>
2019-03-18 21:52:24 +0800

    首页： 标题栏样式和搜索栏样式的基本实现。

--- app/src/main/java/com/example/commoditymanagerment/Fragment/HomepageFragment.java
+++ app/src/main/java/com/example/commoditymanagerment/Fragment/HomepageFragment.java
@@ -1,26 +1,73 @@
 package com.example.commoditymanagerment.Fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.commoditymanagerment.R;
+import com.example.commoditymanagerment.Util.DensityUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class HomepageFragment extends Fragment {
 
-    private View view ;
-    private Context mContext ;
+    @BindView(R.id.tv_setBackText)
+    TextView tvSetBackText;
+    @BindView(R.id.ll_titleLayout)
+    LinearLayout llTitleLayout;
+    @BindView(R.id.et_searchEdit)
+    TextView etSearchEdit;
+    @BindView(R.id.tv_messageEmptyView)
+    TextView tvMessageEmptyView;
+    @BindView(R.id.srl_messageRefreshLayout)
+    SwipeRefreshLayout srlMessageRefreshLayout;
+    Unbinder unbinder;
+    private View view;
+    private Context mContext;
+    private Activity mActivity ;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fg_homepage , container , false) ;
+        view = inflater.inflate(R.layout.fg_homepage, container, false);
+        unbinder = ButterKnife.bind(this, view);
 
-        return view ;
+        mContext = getContext() ;
+        mActivity = getActivity() ;
+        initIndexTitleLayout();
+
+        return view;
+    }
+
+    /**
+     * 设置顶部标题栏相关属性
+     */
+    private void initIndexTitleLayout() {
+        //沉浸式状态栏
+        DensityUtil.fullScreen(mActivity);
+//        设置内边距
+//        其中left right bottom都用现有的
+//        top设置为现在的topPadding+状态栏的高度
+//        表现为将indexTitleLayout显示的数据放到状态栏下面
+        llTitleLayout.setPadding(llTitleLayout.getPaddingLeft(), llTitleLayout.getPaddingTop() + DensityUtil.getStatusHeight(mContext),
+                llTitleLayout.getPaddingRight(), llTitleLayout.getPaddingBottom());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
---
+++ app/src/main/java/com/example/commoditymanagerment/Util/DensityUtil.java
@@ -0,0 +1,65 @@
+package com.example.commoditymanagerment.Util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class DensityUtil {
+
+
+    /**
+     * 获取手机状态栏高度
+     * @param context
+     * @return 状态栏高度
+     */
+    public static int getStatusHeight(Context context) {
+        int statusHeight = -1;
+        try {
+            Class clazz = Class.forName("com.android.internal.R$dimen");
+            Object object = clazz.newInstance();
+            int height = Integer.parseInt(clazz.getField("status_bar_height").get(object).toString());
+            statusHeight = context.getResources().getDimensionPixelSize(height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statusHeight;
+    }
+
+    /**
+     * 沉浸式状态栏
+     * 并且状态栏颜色跟随顶部View的颜色，追随渐变
+     *
+     * @param activity
+     */
+    public static void fullScreen(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                //5.x开始需要把颜色设置透明，否则导航栏会呈现系统默认的浅灰色
+                Window window = activity.getWindow();
+                View decorView = window.getDecorView();
+                //两个 flag 要结合使用，表示让应用的主体内容占用系统状态栏的空间
+                int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                decorView.setSystemUiVisibility(option);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(Color.TRANSPARENT);
+                //导航栏颜色也可以正常设置
+                // window.setNavigationBarColor(Color.TRANSPARENT);
+
+            } else {
+                Window window = activity.getWindow();
+                WindowManager.LayoutParams attributes = window.getAttributes();
+                int flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+                int flagTranslucentNavigation = WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+                attributes.flags |= flagTranslucentStatus;
+                // attributes.flags |= flagTranslucentNavigation;
+                window.setAttributes(attributes);
+            }
+        }
+    }
+
+}
